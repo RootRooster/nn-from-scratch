725d5b32c502cc37a055af7a630c6907c1be1dd3
Nik <62603376+RootRooster@users.noreply.github.com>
2025-03-18 10:00:18 +0100

    moved the core to the main.py

--- network_template.py
+++ network_template.py
@@ -164,24 +164,3 @@ def load_data_cifar(train_file, test_file):
         test_data.transpose(),
         test_class_one_hot.transpose(),
     )
-
-
-if __name__ == "__main__":
-    train_file = "./data/train_data.pckl"
-    test_file = "./data/test_data.pckl"
-    train_data, train_class, test_data, test_class = load_data_cifar(
-        train_file, test_file
-    )
-    val_pct = 0.1
-    val_size = int(train_data.shape[1] * val_pct)
-    val_data = train_data[..., :val_size]
-    val_class = train_class[..., :val_size]
-    train_data = train_data[..., val_size:]
-    train_class = train_class[..., val_size:]
-    # The Network takes as input a list of the numbers of neurons at each layer. The first layer has to match the
-    # number of input attributes from the data, and the last layer has to match the number of output classes
-    # The initial settings are not even close to the optimal network architecture, try increasing the number of layers
-    # and neurons and see what happens.
-    net = Network([train_data.shape[0], 100, 100, 10], optimizer="sgd")
-    net.train(train_data, train_class, val_data, val_class, 20, 64, 0.01)
-    net.eval_network(test_data, test_class)
